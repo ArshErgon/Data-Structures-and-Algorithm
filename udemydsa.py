3c4617d36e7f2670c7c9a392f148cd2ce7e11109
ArshErgon <arshergon@gmail.com>
2021-10-16 15:36:32 +0530

    leetcode solution

--- udemydsa.py
+++ udemydsa.py
@@ -4730,9 +4730,9 @@
 
 
 
-# Merge sort O(n^2)
+# Bubble sort O(n^2)
 
-# class MergeSort:
+# class BubbleSort:
 #     def __init__(self, data):
 #         self.data = data
 
@@ -4744,15 +4744,156 @@
 
 #         return self.data
 
-# obj = MergeSort(list(range(-50, -0)))
+# obj = BubbleSort(list(range(-50, -0)))
 # print(obj.sorting())
 
 
 
 # Selection Sorting O(n^2)
 
-class SelectionSort:
-    def __init__(self, array):
-        self.array = array
+# class SelectionSort:
+#     def __init__(self, array):
+#         self.array = array
 
-    def
+#     def sorting(self):
+#         for i in range(len(self.array)-1):
+#             for x in range(len(self.array)-1):
+#                 if self.array[x] > self.array[x+1]:
+#                     self.array[x], self.array[x+1] = self.array[x+1], self.array[x]
+#         print(self.array)
+
+# obj = SelectionSort([5,4,3,2,1])
+
+# print(obj.sorting())
+
+
+# MergeSort recursion
+
+# class MergeSort:
+
+#     def merge(self, left, right):
+#         result = list()
+#         leftIndex = 0
+#         rightIndex = 0
+
+#         while leftIndex < len(left) and rightIndex < len(right):
+#             if left[leftIndex] < right[rightIndex]:
+#                 result.append(left[leftIndex])
+#                 leftIndex += 1
+#             else:
+#                 result.append(right[rightIndex])
+#                 rightIndex += 1
+
+#         return result    
+
+#     def splitArray(self, array):
+#         mid = (len(array) - 1) // 2
+#         left = array[:mid]
+#         right = array[mid:]
+
+#         print(left, right)
+#         self.merge(self.splitArray(right), self.splitArray(left))
+
+
+# obj = MergeSort()
+
+# print(obj.splitArray([9,8,7,6,5,4,3,2,1]))
+
+# print(obj.merge())
+
+
+# QuickSorting 
+
+
+# # function to find the partition position
+# def partition(array, low, high):
+
+#   # choose the rightmost element as pivot
+#   pivot = array[high]
+
+#   # pointer for greater element
+#   i = low - 1
+
+#   # traverse through all elements
+#   # compare each element with pivot
+#   for j in range(low, high):
+#     if array[j] <= pivot:
+#       # if element smaller than pivot is found
+#       # swap it with the greater element pointed by i
+#       i = i + 1
+
+#       # swapping element at i with element at j
+#       (array[i], array[j]) = (array[j], array[i])
+
+#   # swap the pivot element with the greater element specified by i
+#   (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+#   # return the position from where partition is done
+#   return i + 1
+
+# # function to perform quicksort
+# def quickSort(array, low, high):
+#   if low < high:
+
+#     # find pivot element such that
+#     # element smaller than pivot are on the left
+#     # element greater than pivot are on the right
+#     pi = partition(array, low, high)
+
+#     # recursive call on the left of pivot
+#     quickSort(array, low, pi - 1)
+
+#     # recursive call on the right of pivot
+#     quickSort(array, pi + 1, high)
+
+
+# data = [8, 7, 2, 1, 0, 9, 6]
+# print("Unsorted Array")
+# print(data)
+
+# size = len(data)
+
+# quickSort(data, 0, size - 1)
+
+# print('Sorted Array in Ascending Order:')
+# print(data) 
+
+
+
+
+#  Reverse the words in a string II LeetCode
+
+
+# class ReverseWord:
+#     def __init__(self, string):
+#         self.string = string
+
+#     def reverse(self):
+#         return " ".join(x[::-1] for x in self.string.split())
+
+
+# obj = ReverseWord("Let's take LeetCode contest")
+# print(obj.reverse())
+# print(obj.reverse()=="s'teL ekat edoCteeL tsetnoc")
+# print("s'teL ekat edoCteeL tsetnoc")
+
+
+
+# Reverse a string leetCode
+
+# class ReverseString:
+#     def __init__(self, string, k):
+#         self.string = string
+#         self.k = k
+
+#     def reverse(self):
+#         self.string = list(self.string)
+#         for i in range(0, len(self.string), 2*self.k):
+#         #     self.string[i:i+self.k] = reversed(self.string[i:i+self.k])
+#             print(self.string[i:i+self.k], i)
+#             print(reversed(self.string[i:i+self.k]))
+#         # return ''.join(self.string)
+
+
+# obj = ReverseString("abcdefg", 2)
+# print(obj.reverse())
